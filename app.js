4dd5031dddba2df53f7706378ced1636b49e58b2
AldoTu <aldoturegano10@hotmail.com>
2021-09-25 11:12:57 -0500

    First push

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// Initialize variables
+const app = express();
+
+// CORS
+app.use(cors({ origin: process.env.DOMAIN || '*', allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept', methods: 'GET,POST,PUT,DELETE,OPTIONS' }));
+
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Main route
+const appRoutes = require('./routes/app');
+
+// Import routes
+
+// DB connection
+mongoose.connection.openUri(process.env.MONGODB_URI || 'mongodb://localhost:27017/ionic-app', (err, res) => {
+    if (err) throw err;
+    console.log('DataBase: \x1b[32m%s\x1b[0m', 'online');
+});
+
+// Routes
+
+// Sanity Check
+app.use('/', appRoutes);
+
+// Start listening
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Express server port 3000: \x1b[32m%s\x1b[0m', 'online');
+});
